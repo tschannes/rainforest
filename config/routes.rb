4a86f21a3c55a06bca1f8014d89410eba8655f1e
tschannes <mail@tscherrig.net>
2013-07-08 13:43:15 -0400

    User model added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rainforest::Application.routes.draw do
+  get "users/new"
+  get "users/create"
   root 'products#index'
   
   resources :products
+  resources :users, :only => [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

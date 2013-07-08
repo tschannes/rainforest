9b801639d1c69ad36bc4098593f93ae0d6d6d4e0
tschannes <mail@tscherrig.net>
2013-07-08 17:01:28 -0400

    user profile added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
+  
   def new
     @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def create
@@ -10,6 +15,21 @@ class UsersController < ApplicationController
       redirect_to products_url, :notice => "Signed up!"
     else
       render "new"
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    params.permit!
+    @user = User.find(params[:id])
+    
+    if @user.update_attributes(params[:user])
+      redirect_to products_url, :notice => "Profile was successfully updated"
+    else
+      render "show"
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :email, :on => :create
   validates_presence_of :password, :on => :create
+  validates_presence_of :name
 
   has_many :reviews
   has_many :products, :through => :reviews

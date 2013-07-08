78dcfb38e7b013e00d7a134831d0f571df3ef065
tschannes <mail@tscherrig.net>
2013-07-08 14:25:17 -0400

    Review added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :email, :on => :create
   validates_presence_of :password, :on => :create
+
+  has_many :reviews
+  has_many :products, :through => :reviews
 end
